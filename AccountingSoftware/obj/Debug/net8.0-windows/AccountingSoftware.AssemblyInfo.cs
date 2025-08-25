1bf4491b677ab7b7a0eabe57747ad518bddb982d
yongqin123 <yongqin12324@gmail.com>
2025-08-25 18:45:59 +0800

    update SFP

--- AccountingSoftware/obj/Debug/net8.0-windows/AccountingSoftware.AssemblyInfo.cs
+++ AccountingSoftware/obj/Debug/net8.0-windows/AccountingSoftware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountingSoftware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b9143d9a93d9190e1f4c5b3381ecf94cb0ccb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ce653657c02646524645ac5e56dfdd5a8d8aa4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountingSoftware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountingSoftware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
